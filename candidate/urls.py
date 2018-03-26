c26edf97689c1fd67da21d8b7bbc17c6f9214f83
Thykof <nathan.seva@outlook.fr>
2018-03-26 21:57:47 +0200

    Add app name url

--- candidate/urls.py
+++ candidate/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from candidate import views
 
 
+app_name = 'candidate'
 urlpatterns = [
     path('connexion', views.login, name='login'),
     path('inscription', views.signin, name='signin'),
