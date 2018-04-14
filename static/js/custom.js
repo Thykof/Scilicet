128f780e35670d4eb24ffe0bafe40fefd313dcd3
Thykof <nathan.seva@outlook.fr>
2018-04-14 11:55:37 +0200

    Word carousel

---
+++ static/js/custom.js
@@ -0,0 +1,10 @@
+/*
+$(".nav li").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
+*/
+
+$('.carousel').carousel({
+  interval: 1700
+})
