d5c9258086f0b7ea71bcdcf3a3f54d76718d95e3
Thykof <nathan.seva@outlook.fr>
2018-04-23 22:37:55 +0200

    Link profile-list recruitment

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -3,12 +3,12 @@ from django.views.generic import TemplateView
 
 
 from recruitment import views
+from candidate import views as candidate_views
 
 
 app_name = 'recruitment'
 urlpatterns = [
     path('', TemplateView.as_view(template_name="recruitment/recruitment.html"), name='recruitment'),
     path('a-propos', TemplateView.as_view(template_name="recruitment/about.html"), name='about'),
-    path('accueil', TemplateView.as_view(template_name="recruitment/home.html"), name="home"),
-    path('recherche', views.search, name="search"),
+    path('recherche', candidate_views.ProfileList.as_view(), name="search"),
 ]
